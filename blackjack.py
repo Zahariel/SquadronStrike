88799805991d69588c763735aac5b14030140d0d
Paul Zagieboylo <paul@zag.net>
2021-02-11 22:06:35 -0600

    fixed blackjack simulator

--- blackjack.py
+++ blackjack.py
@@ -56,7 +56,7 @@ def do_statistics(distribution, limit=30):
     total = sum(distribution.values())
     rescaled = dict(sorted((key, f"{(num * 100) // successes}%") for key, num in distribution.items() if key < limit))
     average = sum(key * num for key, num in distribution.items()) / total
-    variance = sum((key - average)**2 * num for key, num in distribution.items) / total
+    variance = sum((key - average)**2 * num for key, num in distribution.items()) / total
     stddev = math.sqrt(variance)
     return average, stddev, rescaled
 
@@ -65,17 +65,14 @@ for threshold in [17,18,19,20,21]:
     TRIALS = 100000
     successes, cards_until_success, cards_until_bust, cards_in_successful_hands, cards_in_busted_hands = trials(threshold, TRIALS)
 
+    total_success_cards_avg, total_success_cards_stddev, total_success_cards_raw = do_statistics(cards_until_success)
+    total_bust_cards_avg, total_bust_cards_stddev, total_bust_cards_raw = do_statistics(cards_until_bust)
+    success_cards_avg, success_cards_stddev, success_cards_raw = do_statistics(cards_in_successful_hands)
+    bust_cards_avg, bust_cards_stddev, bust_cards_raw = do_statistics(cards_in_busted_hands)
 
-
-    total_success_cards_rescaled = dict(sorted((card_count, f"{(num * 100) // successes}%") for card_count, num in cards_until_success.items() if card_count < 30))
-    average_total_success = sum(card_count * num for card_count, num in cards_until_success) / successes
-
-    total_bust_cards_rescaled = dict(sorted((card_count, f"{(num * 100) // (TRIALS - successes)}%") for card_count, num in cards_until_bust.items() if card_count < 30))
-    success_cards_rescaled = dict(sorted((card_count, f"{(num * 100) // successes}%") for card_count, num in cards_in_successful_hands.items()))
-    busted_cards_rescaled = dict(sorted((card_count, f"{(num * 100) // (TRIALS - successes)}%") for card_count, num in cards_in_busted_hands.items()))
     print(f"{threshold}: {(successes * 100) // TRIALS}% successful hands")
-    print(f"    total cards until success: {total_success_cards_rescaled}")
-    print(f"    total cards until bust:    {total_bust_cards_rescaled}")
-    print(f"    cards in successful hands: {success_cards_rescaled}")
-    print(f"    cards in busted hands:     {busted_cards_rescaled}")
+    print(f"    total cards until success: {total_success_cards_avg:.2f} +/- {total_success_cards_stddev:.2f} (raw: {total_success_cards_raw})")
+    print(f"    total cards until bust:    {total_bust_cards_avg:.2f} +/- {total_bust_cards_stddev:.2f} (raw: {total_bust_cards_raw})")
+    print(f"    cards in successful hands: {success_cards_avg:.2f} +/- {success_cards_stddev:.2f} (raw: {success_cards_raw})")
+    print(f"    cards in busted hands:     {bust_cards_avg:.2f} +/- {bust_cards_stddev:.2f} (raw: {bust_cards_raw})")
 
